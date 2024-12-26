05e462af340d70d320f5593dd7ba9546fa2edff2
Suha747 <mariamsuha8@gmail.com>
2024-12-26 09:47:02 +0500

    Add files via upload
    HomeFlow is a sleek desktop application for managing smart home devices. Built with WPF and XAML, it provides an intuitive interface to control lighting, temperature, security, and more.

---
+++ usercontrols/AddButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace HomeFlow.usercontrols
+{
+
+    public partial class AddButton : UserControl
+    {
+        public AddButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(AddButton));
+
+    }
+}
